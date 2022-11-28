53895d5af2c4e7b661e26930725d975aacae8441
Lê Nhật Minh <81094010+lnm451@users.noreply.github.com>
2022-11-28 22:49:43 +0700

    Footer

---
+++ src/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/index.css">
+
+    <!-- Bootstrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Icon Package -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+    <!-- FA Package -->
+    <script src="https://kit.fontawesome.com/8accce40a8.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <?php
+    include('footer.html');
+    ?>
+</body>
+
+</html>
