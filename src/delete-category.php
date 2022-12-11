9448ac9a9eb5c6013d740a82ea122c336f9d212f
Minh Nhật Mai <nhat.maik20dhbk@hcmut.edu.vn>
2022-12-11 23:58:49 +0700

    Finalize test deletion

--- src/delete-category.php
+++ src/delete-category.php
@@ -7,9 +7,9 @@
     else {
         $getID = $_GET['id'];
         if(!empty($getID)) {
-            $delete_sql = "DELETE FROM Category where ID='$getID'";
-            mysqli_query($conn, $delete_sql);
             $delete_sql = "DELETE FROM Test_Cate where C_ID='$getID'";
+            mysqli_query($conn, $delete_sql);
+            $delete_sql = "DELETE FROM Category where ID='$getID'";
             mysqli_query($conn, $delete_sql);
         }
         header("Location: index.php?page=categories");
--- src/delete-test.php
+++ src/delete-test.php
@@ -7,13 +7,16 @@
     else {
         $getID = $_GET['id'];
         if(!empty($getID)) {
-            $delete_sql = "DELETE FROM Test where ID='$getID'";
+            $delete_sql = "DELETE FROM User_Test where T_ID='$getID'";
             mysqli_query($conn, $delete_sql);
-            $delete_sql = "DELETE FROM Test_Cate where T_ID='$getID'";
-            mysqli_query($conn, $delete_sql);
+            
             $delete_sql = "DELETE FROM Question where T_ID='$getID'";
             mysqli_query($conn, $delete_sql);
-            $delete_sql = "DELETE FROM User_Test where T_ID='$getID'";
+
+            $delete_sql = "DELETE FROM Test_Cate where T_ID='$getID'";
+            mysqli_query($conn, $delete_sql);
+
+            $delete_sql = "DELETE FROM Test where ID='$getID'";
             mysqli_query($conn, $delete_sql);
         }
         header("Location: index.php?page=library");
